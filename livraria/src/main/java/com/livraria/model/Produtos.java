4d1399d22031c141838c079f9c085b11dc927f76
EliasJuk <eliasp.juk@hotmail.com>
2022-09-25 09:28:34 -0300

    mapeado a classe Produtos

--- livraria/src/main/java/com/livraria/model/Produtos.java
+++ livraria/src/main/java/com/livraria/model/Produtos.java
@@ -1,7 +1,19 @@
 package com.livraria.model;
 
-public class Produtos {  
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name = "produtos")
+public class Produtos {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+
   private String nome;
   private float peso;
   private String descricao;
